4608d2601555a8674c4b01fd9d5c4e77fe36c5fa
SirRGB <sirrgb@proton.me>
2024-05-21 00:57:06 +0200

    cedric: Sync extract utils with template
    Change-Id: Ia49be31d4f9e3c9024a4b48a56a07b20c46cb795

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,70 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-fpcservice)
+            sed -i 's|/firmware/image|/vendor/f/image|' "${2}"
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so"
+            ;;
+
+        vendor/lib/hw/camera.msm8937.so)
+            "${PATCHELF}" --set-soname "camera.msm8937.so" "${2}"
+            ;;
+
+        vendor/lib/com.fingerprints.extension@1.0_vendor.so)
+            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+            ;;
+
+        vendor/lib/libactuator_dw9767_truly.so)
+            "${PATCHELF}" --set-soname "libactuator_dw9767_truly.so" "${2}"
+            ;;
+
+        vendor/lib/libDepthBokehEffect.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+
+        vendor/lib/libDepthBokehEffect2.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+
+        vendor/lib/libmmcamera2_pproc_modules.so)
+            sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
+            sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${2}"
+            ;;
+
+        vendor/lib/libmmcamera_ppeiscore.so)
+            sed -i "s/libgui/libwui/" "${2}"
+            "${PATCHELF}" --add-needed "libppeiscore_shim.so" "${2}"
+            ;;
+
+        vendor/lib/libmmcamera_hdr_gb_lib.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+
+        vendor/lib/liboptizoom.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+
+        vendor/lib/libseemore.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+
+        vendor/lib/libtrueportrait.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+
+        vendor/lib/libubifocus.so)
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+
+        vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc)
+            sed -i "s/system input/system uhid input/" "${2}"
+            sed -i "s/class late_start/class hal/" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
@@ -19,29 +83,3 @@ export DEVICE=cedric
 export VENDOR=motorola
 
 "./../../${VENDOR}/${BOARD_COMMON}/extract-files.sh" "$@"
-
-DEVICE_BLOB_ROOT="../../../vendor/${VENDOR}/${DEVICE}/proprietary"
-
-sed -i 's|/firmware/image|/vendor/f/image|' "${DEVICE_BLOB_ROOT}/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-fpcservice"
-"${PATCHELF}" --set-soname camera.msm8937.so "${DEVICE_BLOB_ROOT}/vendor/lib/hw/camera.msm8937.so"
-"${PATCHELF}" --remove-needed android.hidl.base@1.0.so "${DEVICE_BLOB_ROOT}/vendor/lib/com.fingerprints.extension@1.0_vendor.so"
-"${PATCHELF}" --set-soname libactuator_dw9767_truly.so "${DEVICE_BLOB_ROOT}/vendor/lib/libactuator_dw9767_truly.so"
-sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera2_pproc_modules.so"
-sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera2_sensor_modules.so"
-sed -i "s/libgui/libwui/" "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera_ppeiscore.so"
-"${PATCHELF}" --add-needed libppeiscore_shim.so "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera_ppeiscore.so"
-sed -i "s/system input/system uhid input/" "${DEVICE_BLOB_ROOT}/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc"
-sed -i "s/class late_start/class hal/" "${DEVICE_BLOB_ROOT}/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc"
-
-# Replace libstdc++.so with libstdc++_vendor.so
-"${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${DEVICE_BLOB_ROOT}/vendor/lib/libDepthBokehEffect.so"
-"${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${DEVICE_BLOB_ROOT}/vendor/lib/libDepthBokehEffect2.so"
-"${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${DEVICE_BLOB_ROOT}/vendor/lib/libchromaflash.so"
-"${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera_hdr_gb_lib.so"
-"${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${DEVICE_BLOB_ROOT}/vendor/lib/liboptizoom.so"
-"${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${DEVICE_BLOB_ROOT}/vendor/lib/libseemore.so"
-"${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${DEVICE_BLOB_ROOT}/vendor/lib/libtrueportrait.so"
-"${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${DEVICE_BLOB_ROOT}/vendor/lib/libubifocus.so"
-
-# Use libhidlbase-v32 for select Android P blobs
-"${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${DEVICE_BLOB_ROOT}/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-fpcservice"
